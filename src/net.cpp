946ff9bca4481b0637d11cc8d5f4291b18255260
Bitmxittz <bitmxittz@gmail.com>
2017-10-23 19:43:36 +0530

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1064,9 +1064,9 @@ void ThreadMapPort()
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
 #else
-    /* miniupnpc 1.6 */
+    /* miniupnpc 1.9 */
     int error = 0;
-    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
     struct UPNPUrls urls;
